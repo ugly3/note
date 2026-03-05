50a14c49505c883f5cfd1611d77748f507f23852
ugly3 <619755492@qq.com>
2026-03-05 19:13:40 +0800

    vault backup: 2026-03-05 19:13:40

--- competition/codehxxx_CAR11/OUTPUT/startup_stm32f40_41xxx.d
+++
@@ -1,1 +0,0 @@
-..\output\startup_stm32f40_41xxx.o: ..\CMSIS\startup_stm32f40_41xxx.s
--- competition/codehxxx_CAR11/OUTPUT/swopt_drv.d
+++
@@ -1,4 +0,0 @@
-..\output\swopt_drv.o: ..\my_lib\swopt_drv.c
-..\output\swopt_drv.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
-..\output\swopt_drv.o: ..\HARDWARE\CAN\fifo_drv.h
-..\output\swopt_drv.o: ..\my_lib\swopt_drv.h
