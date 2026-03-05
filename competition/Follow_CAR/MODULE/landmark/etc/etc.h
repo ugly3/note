6aed88854f3d64df6fbb86cb997dc3f818e2d6b6
ugly3 <619755492@qq.com>
2026-03-06 00:29:41 +0800

    vault backup: 2026-03-06 00:29:40

--- competition/Follow_CAR/MODULE/landmark/etc/etc.h
+++ competition/Follow_CAR/MODULE/landmark/etc/etc.h
@@ -2,8 +2,8 @@
 #define __ETC_H
 #include "all_module.h"
 
-#define ETC_Middle 1	//ETC在中间
-#define ETC_Cross 0	//ETC在十字路口
+#define ETC_Middle 0 // ETC在中间
+#define ETC_Cross 1	 // ETC在十字路口
 
 typedef struct
 {
@@ -11,22 +11,16 @@ typedef struct
 	uint8_t Left_Gate_Dowm;
 	uint8_t Right_Gate_Up;
 	uint8_t Right_Gate_Dowm;
-    uint8_t ETC_Car_Start;
+	uint8_t ETC_Car_Start;
 	void (*xETC_Init_Angle_Adjust)(uint8_t);
 	void (*xETC_Pass)(void);
-    void (*xETC_Pass_RFID)(uint8_t,uint8_t,uint8_t);
-}ETC_Typedef;
+	void (*xETC_Pass_RFID)(uint8_t, uint8_t, uint8_t);
+} ETC_Typedef;
 
 extern ETC_Typedef ETC_Data;
 
-
-
-
 void xETC_Init_Angle_Adjust(uint8_t AngleAdjust);
 void xETC_Pass(void);
-void xETC_Pass_RFID(uint8_t card1,uint8_t card2,uint8_t card3);
-
-
-
+void xETC_Pass_RFID(uint8_t card1, uint8_t card2, uint8_t card3);
 
 #endif
