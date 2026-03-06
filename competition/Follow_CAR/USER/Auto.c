6bf03df05d592b017e0d0be43791a573452892fa
ugly3 <619755492@qq.com>
2026-03-06 20:31:42 +0800

    vault backup: 2026-03-06 20:31:42

--- competition/Follow_CAR/USER/Auto.c
+++ competition/Follow_CAR/USER/Auto.c
@@ -456,7 +456,7 @@ void xAuto_Run_Function(void)
     {
       delay_ms(500);
       Count_ms = 0;
-      Motor_Data.xCAR_Go(25, 250);                // 使车身对准十字路口
+      Motor_Data.xCAR_Go(25, 100);                // 使车身对准十字路口
       delay_ms(500);
       Motor_Data.xCAR_Track(Motor_Data.Go_speed); // 循迹
 
