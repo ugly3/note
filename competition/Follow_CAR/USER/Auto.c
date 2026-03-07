b110dff033f2ef8fd069ccecc65d83f160a27708
ugly3 <619755492@qq.com>
2026-03-07 20:15:38 +0800

    vault backup: 2026-03-07 20:15:38

--- competition/Follow_CAR/USER/Auto.c
+++ competition/Follow_CAR/USER/Auto.c
@@ -448,8 +448,8 @@ void xAuto_Run_Function(void)
   {
 
     Motor_Data.xCAR_Track_Go();
-    delay_ms(200); // B6
     Motor_Data.xCAR_L45(wheel_Speed, wheel_Time);
+    delay_ms(200);
     Identify_Two_Code_Arr[3] = 0x01;
     Android_Data.xMainCar_Send_Android(Identify_Two_Code_Arr); // 发送请求识别二维码
     delay_ms(500);
@@ -459,13 +459,13 @@ void xAuto_Run_Function(void)
 
     if (Android_Data.Two_Code_State == 0) // 如果未进入识别模式
     {
-      Motor_Data.xCAR_Back(20,200);
+      Motor_Data.xCAR_Back(20, 200);
       Rx_count = 0;
       Android_Data.xMainCar_Send_Android(Identify_Two_Code_Arr); // 发送请求识别二维码
       delay_ms(500);
       delay_ms(500);
       delay_ms(500);
-      Motor_Data.xCAR_Go(20,200);
+      Motor_Data.xCAR_Go(20, 200);
     }
     if (Android_Data.Two_Code_State == 0)
     {
@@ -479,10 +479,8 @@ void xAuto_Run_Function(void)
       YT1_parse_two_codes();
 
       printf("sdv\r\n");
-      printf("%s\r\n",Two_Code_Data_parsed_Store1);
-         printf("%s\r\n",Two_Code_Data_parsed_Store2);
-
-      
+      printf("%s\r\n", Two_Code_Data_parsed_Store1);
+      printf("%s\r\n", Two_Code_Data_parsed_Store2);
     }
     // mart_Traffic_Data.xSmart_Traffic_Ask_State(Smart_Traffic_Data.Device_A);  //请求交通灯进入识别模式，并请求安卓识别红绿灯
     Run_State = 2;
