c5d2ccccd501bc739efd17d0b93b52117b54f1fb
ugly3 <619755492@qq.com>
2026-03-08 00:14:16 +0800

    vault backup: 2026-03-08 00:14:16

--- competition/Follow_CAR/USER/Auto.c
+++ competition/Follow_CAR/USER/Auto.c
@@ -526,7 +526,7 @@ void xAuto_Run_Function(void)
         // 假设任务需要将字符 'D' 和 '7' 识别为十六进制数值
         // 这里提供一种通用的字符转数值方法
 
-//转成十六进制，无线充电标志物开启码
+        // 转成十六进制，无线充电标志物开启码
         for (int i = 0; i < strlen(Two_Code_Data_parsed_Store2); i++)
         {
           char c = Two_Code_Data_parsed_Store2[i];
@@ -538,19 +538,19 @@ void xAuto_Run_Function(void)
             Hex_Data_Store1[i] = c - 'a' + 10;
         }
       }
-      Send_wireless_open_To_MainCar[3]= Hex_Data_Store1[0];
-      Send_wireless_open_To_MainCar[4]= Hex_Data_Store1[1];
-      Send_wireless_open_To_MainCar[5]= Hex_Data_Store1[2];
+      Send_wireless_open_To_MainCar[3] = Hex_Data_Store1[0];
+      Send_wireless_open_To_MainCar[4] = Hex_Data_Store1[1];
+      Send_wireless_open_To_MainCar[5] = Hex_Data_Store1[2];
       MainCar_Data.xSend_Command_To_MainCar(Send_wireless_open_To_MainCar);
       delay_ms(100);
       MainCar_Data.xSend_Command_To_MainCar(Send_wireless_open_To_MainCar);
 
-     Send_Weizhi_To_MainCar[3] = (CharToHex(Two_Code_Data_parsed_Store1[0]) << 4) | 
-                     CharToHex(Two_Code_Data_parsed_Store1[1]);
-    MainCar_Data.xSend_Command_To_MainCar(Send_Weizhi_To_MainCar);
+      Send_Weizhi_To_MainCar[3] = (CharToHex(Two_Code_Data_parsed_Store1[0]) << 4) |
+                                  CharToHex(Two_Code_Data_parsed_Store1[1]);
+      MainCar_Data.xSend_Command_To_MainCar(Send_Weizhi_To_MainCar);
       delay_ms(100);
       MainCar_Data.xSend_Command_To_MainCar(Send_Weizhi_To_MainCar);
-
+      delay_ms(100);
     }
     Run_State = 2;
     break;
