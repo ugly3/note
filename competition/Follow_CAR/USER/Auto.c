314a301a418070864ed7e28725a19f1f8a879ace
ugly3 <619755492@qq.com>
2026-03-08 00:31:54 +0800

    vault backup: 2026-03-08 00:31:54

--- competition/Follow_CAR/USER/Auto.c
+++ competition/Follow_CAR/USER/Auto.c
@@ -444,22 +444,22 @@ uint8_t Buf1[200];
 uint8_t Hex_Data_Store1[10];
 void xAuto_Run_Function(void)
 {
-  switch (Run_State)
-  {
-  case 1:
-  {
-    while (Communication_Data.FollowCar_Start_flag == 0)
+   while (Communication_Data.FollowCar_Start_flag == 0)
     {
       delay_ms(500);
       delay_ms(500);
       time_out++;
       if (time_out > 230) // 等待10秒后超时
       {
+        Run_State=1; // 设置状态为1，进入下一阶段
         time_out = 0; // 重置超时计数器
         break;        // 跳出循环，继续执行后续代码
       }
     }
-
+  switch (Run_State)
+  {
+  case 1:
+  {
     Motor_Data.xCAR_Track_Go();
     Motor_Data.xCAR_L45(wheel_Speed, wheel_Time);
     delay_ms(200);
