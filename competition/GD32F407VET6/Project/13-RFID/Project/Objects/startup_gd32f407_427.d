1a9bac29a64f092ed4dd87c229d61c176cef5f65
ugly3 <619755492@qq.com>
2026-03-09 23:24:29 +0800

    vault backup: 2026-03-09 23:24:29

--- competition/GD32F407VET6/Project/13-RFID/Project/Objects/startup_gd32f407_427.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_gd32f407_427.o: ..\Firmware\CMSIS\GD\GD32F4xx\Source\ARM\startup_gd32f407_427.s
--- competition/GD32F407VET6/Project/5-light/删除目标文件(用于打包备份).bat
+++
@@ -1,5 +0,0 @@
-rd /Q /S Project\Listings
-rd /Q /S Project\Objects
-del /Q Project\project.uvgui.*
-
-
