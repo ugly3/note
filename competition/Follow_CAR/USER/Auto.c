6b92fb3258dcb8a1b9b06e9ed99a3c56ccee6569
ugly3 <619755492@qq.com>
2026-03-10 21:22:03 +0800

    vault backup: 2026-03-10 21:22:03

--- competition/Follow_CAR/USER/Auto.c
+++ competition/Follow_CAR/USER/Auto.c
@@ -1176,6 +1176,10 @@ void xAuto_Run_Function(void)
 
   case 2:
   {
+    Motor_Data.xCAR_Track_Go();
+    Motor_Data.xCAR_Track_Time(35, 700);
+    Motor_Data.xCAR_Track_Go();//D4
+    Motor_Data.xCAR_L90(wheel_Speed, wheel_Time * 2);
 
     Run_State = 3;
     break;
