4daec960bbd77a5ecaab4f684234f3313e721db5
ugly3 <619755492@qq.com>
2026-03-10 22:11:32 +0800

    vault backup: 2026-03-10 22:11:32

--- competition/Follow_CAR/USER/Auto.c
+++ competition/Follow_CAR/USER/Auto.c
@@ -1233,7 +1233,13 @@ void xAuto_Run_Function(void)
       delay_ms(500);
     }
     Motor_Data.xCAR_L90(wheel_Speed, wheel_Time * 2);
+    delay_ms
     Motor_Data.xCAR_Track_Go(); // D2
+    Motor_Data.xCAR_R90(wheel_Speed, wheel_Time * 2);
+    delay_ms(200);
+    Motor_Data.xCAR_Track_Go(); // F2
+
+
     Run_State = 4;
     break;
   }
