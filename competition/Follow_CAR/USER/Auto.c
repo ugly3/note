6be2a641a6f09bb2887f1dc40b2db6057beafe43
ugly3 <619755492@qq.com>
2026-03-10 14:22:35 +0800

    vault backup: 2026-03-10 14:22:35

--- competition/Follow_CAR/USER/Auto.c
+++ competition/Follow_CAR/USER/Auto.c
@@ -987,43 +987,7 @@ void xAuto_Run_Function(void)
       printf("sdv\r\n");
       printf("%s\r\n", Two_Code_Data_parsed_Store1);
       printf("%s\r\n", Two_Code_Data_parsed_Store2);
-      if (strlen((const char *)Two_Code_Data_parsed_Store1) > 0)
-      {
-        // 假设任务需要将字符 'D' 和 '7' 识别为十六进制数值
-        // 这里提供一种通用的字符转数值方法
-
-        // 转成十六进制，无线充电标志物开启码
-        for (int i = 0; i < strlen((char *)Two_Code_Data_parsed_Store2); i++)
-        {
-          char c = Two_Code_Data_parsed_Store2[i];
-          if (c >= '0' && c <= '9')
-            Hex_Data_Store1[i] = c - '0';
-          else if (c >= 'A' && c <= 'F')
-            Hex_Data_Store1[i] = c - 'A' + 10;
-          else if (c >= 'a' && c <= 'f')
-            Hex_Data_Store1[i] = c - 'a' + 10;
-        }
-      }
-      Send_wireless_open_To_MainCar[3] = Hex_Data_Store1[0];
-      Send_wireless_open_To_MainCar[4] = Hex_Data_Store1[1];
-      Send_wireless_open_To_MainCar[5] = Hex_Data_Store1[2];
-      MainCar_Data.xSend_Command_To_MainCar(Send_wireless_open_To_MainCar);
-      delay_ms(100);
-      MainCar_Data.xSend_Command_To_MainCar(Send_wireless_open_To_MainCar);
-
-      if (strcmp((const char *)Two_Code_Data_parsed_Store1, "D7") == 0)
-        Send_Weizhi_To_MainCar[3] = 0x01;
-      else if (strcmp((const char *)Two_Code_Data_parsed_Store1, "F7") == 0)
-        Send_Weizhi_To_MainCar[3] = 0x02;
-      else if (strcmp((const char *)Two_Code_Data_parsed_Store1, "G6") == 0) // G6
-        Send_Weizhi_To_MainCar[3] = 0x03;
-      else // G4
-        Send_Weizhi_To_MainCar[3] = 0x04;
-
-      MainCar_Data.xSend_Command_To_MainCar(Send_Weizhi_To_MainCar);
-      delay_ms(100);
-      MainCar_Data.xSend_Command_To_MainCar(Send_Weizhi_To_MainCar);
-      delay_ms(100);
+     
     }
     Run_State = 2;
     break;
