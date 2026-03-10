ad833dba82025b1b23a815cf61c45e6bb3e3203c
ugly3 <619755492@qq.com>
2026-03-10 14:27:10 +0800

    vault backup: 2026-03-10 14:27:10

--- competition/Follow_CAR/USER/Auto.c
+++ competition/Follow_CAR/USER/Auto.c
@@ -987,14 +987,17 @@ void xAuto_Run_Function(void)
       printf("sdv\r\n");
       printf("%s\r\n", Two_Code_Data_parsed_Store1);
       printf("%s\r\n", Two_Code_Data_parsed_Store2);
-     
     }
+    Motor_Data.xCAR_R90(wheel_Speed, wheel_Time * 2);
+    Motor_Data.xCAR_R45(wheel_Speed, wheel_Time * 2);
     Run_State = 2;
     break;
   }
   case 2:
   {
-
+    Motor_Data.xCAR_Track_Go(); // F4
+    Motor_Data.xCAR_L90(wheel_Speed, wheel_Time * 2);
+    
     Run_State = 3;
     break;
   }
