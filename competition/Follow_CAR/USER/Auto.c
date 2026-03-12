afe3ee1fef45b80ada8b345d9afb52f0adee41d8
ugly3 <619755492@qq.com>
2026-03-12 22:07:41 +0800

    vault backup: 2026-03-12 22:07:41

--- competition/Follow_CAR/USER/Auto.c
+++ competition/Follow_CAR/USER/Auto.c
@@ -1343,7 +1343,7 @@ void xAuto_Run_Function(void)
     delay_ms(500);
     Motor_Data.xCAR_Track_Go(); // D2
     delay_ms(400);
-    printf("Init_light:% d\r\n", MainCar_Send_Init_light);
+    printf("Init_light:%d\r\n", MainCar_Send_Init_light);
 
     if (MainCar_Send_Init_light % 2 + 1 == 1) // Á¢Ìå³µ¿â
     {
