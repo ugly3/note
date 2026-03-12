326657bb443ba3102eeab9df129912ba7f4b6dd9
ugly3 <619755492@qq.com>
2026-03-13 06:51:15 +0800

    vault backup: 2026-03-13 06:51:15

--- competition/Follow_CAR/USER/Auto.c
+++ competition/Follow_CAR/USER/Auto.c
@@ -605,9 +605,9 @@ void xAuto_Run_Function(void)
       delay_ms(300);
       Motor_Data.xCAR_Track(20);
       delay_ms(300);
-      Motor_Data.xCAR_Go(30, 610); // ³å
+      Motor_Data.xCAR_Go(30, 620); // ³å
       delay_ms(100);
-      Motor_Data.xCAR_Go(30, 610); // ³å
+      Motor_Data.xCAR_Go(30, 620); // ³å
       delay_ms(500);
       Motor_Data.xCAR_Track_Go();
       Motor_Data.xCAR_Track_Go();
@@ -616,9 +616,9 @@ void xAuto_Run_Function(void)
     else if (1200 > Count_ms) // D2
     {
       Count_ms = 0;
-      Motor_Data.xCAR_Go(30, 610); // ³å
+      Motor_Data.xCAR_Go(30, 620); // ³å
       delay_ms(100);
-      Motor_Data.xCAR_Go(30, 610); // ³å
+      Motor_Data.xCAR_Go(30, 620); // ³å
       delay_ms(300);
       Motor_Data.xCAR_Track_Go();
       delay_ms(200);
@@ -632,9 +632,9 @@ void xAuto_Run_Function(void)
       delay_ms(500);
       Motor_Data.xCAR_Back(20, 600);
       delay_ms(300);
-      Motor_Data.xCAR_Go(30, 610); // ³å
+      Motor_Data.xCAR_Go(30, 620); // ³å
       delay_ms(100);
-      Motor_Data.xCAR_Go(30, 610); // ³å
+      Motor_Data.xCAR_Go(30, 620); // ³å
       delay_ms(500);
 
       Motor_Data.xCAR_Track_Go();
@@ -842,7 +842,7 @@ void xAuto_Run_Function(void)
   {
     delay_ms(200);
     track_time_Start = 1;
-    Motor_Data.xCAR_Track(Motor_Data.Go_speed); // Ñ­¼£
+    Motor_Data.xCAR_Track(40); // Ñ­¼£
     delay_ms(200);
     sprintf((char *)Buf1, "Count_ms:%d\r\n ", Count_ms);
     Send_InfoData_To_Fifo((char *)Buf1, strlen((char *)Buf1));
@@ -854,9 +854,9 @@ void xAuto_Run_Function(void)
       delay_ms(300);
       Motor_Data.xCAR_Track(30);
       delay_ms(300);
-      Motor_Data.xCAR_Go(30, 610); // ³å
+      Motor_Data.xCAR_Go(30, 620); // ³å
       delay_ms(100);
-      Motor_Data.xCAR_Go(30, 610); // ³å
+      Motor_Data.xCAR_Go(30, 620); // ³å
       delay_ms(300);
       Motor_Data.xCAR_Track_Go();
       Motor_Data.xCAR_Track_Go();
@@ -865,9 +865,9 @@ void xAuto_Run_Function(void)
     else if (1200 > Count_ms) // D2
     {
       Count_ms = 0;
-      Motor_Data.xCAR_Go(30, 610); // ³å
+      Motor_Data.xCAR_Go(30, 620); // ³å
       delay_ms(100);
-      Motor_Data.xCAR_Go(30, 610); // ³å
+      Motor_Data.xCAR_Go(30, 620); // ³å
       delay_ms(300);
       Motor_Data.xCAR_Track_Go();
       delay_ms(200);
@@ -882,9 +882,9 @@ void xAuto_Run_Function(void)
       Motor_Data.xCAR_Back(20, 600);
       delay_ms(300);
       Motor_Data.xCAR_Track(20);
-      Motor_Data.xCAR_Go(30, 610); // ³å
+      Motor_Data.xCAR_Go(30, 620); // ³å
       delay_ms(100);
-      Motor_Data.xCAR_Go(30, 610); // ³å
+      Motor_Data.xCAR_Go(30, 620); // ³å
       delay_ms(300);
 
       Motor_Data.xCAR_Track_Go();
@@ -1526,7 +1526,7 @@ void xAuto_Run_Function(void)
     uint8_t Buf1[200];
     delay_ms(200);
     track_time_Start = 1;
-    Motor_Data.xCAR_Track(30); // Ñ­¼£
+    Motor_Data.xCAR_Track(40); // Ñ­¼£
     delay_ms(200);
     sprintf((char *)Buf1, "Count_ms:%d\r\n ", Count_ms);
     Send_InfoData_To_Fifo((char *)Buf1, strlen((char *)Buf1));
