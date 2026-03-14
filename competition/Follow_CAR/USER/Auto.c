acacd2f7f5cc38a8233cde7fdfe25f1249d0ad74
ugly3 <619755492@qq.com>
2026-03-14 09:32:20 +0800

    vault backup: 2026-03-14 09:32:20

--- competition/Follow_CAR/USER/Auto.c
+++ competition/Follow_CAR/USER/Auto.c
@@ -1534,7 +1534,7 @@ void xAuto_Run_Function(void)
   }
   case 2:
   {
-    Motor_Data.xCAR_L45(wheel_Speed, wheel_Time);
+    Motor_Data.xCAR_L45(wheel_Speed + 10, wheel_Time);
     delay_ms(200);
     // ¶þÎ¬ÂëÊ¶±ð
     Identify_Two_Code_Arr[3] = 0x01;
